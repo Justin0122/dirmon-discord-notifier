59cb8b4fe46c988074c3198ba2a07d7d11f2b360
Justin Jongstra <00300061@rijnijssel.nl>
2023-03-05 22:15:44 +0100

    added ssh detection

--- dirmon.py
+++ dirmon.py
@@ -1,15 +1,24 @@
 import os
 import subprocess
-import requests
 import json
 import time
 import cv2
 from config import DIR
 from config import WEBHOOK_URL
 
+if subprocess.call(["which", "inotifywait"], stdout=subprocess.PIPE) != 0:
+    print("Installing inotify-tools...")
+    subprocess.call(["sudo", "pacman", "-S", "inotify-tools"])
+
+try:
+    import requests
+except ImportError:
+    print("Installing requests...")
+    subprocess.call(["pip3", "install", "requests"])
+
+
 webhook_url = WEBHOOK_URL
 
-# Replace this with the path to the directory you want to monitor
 dir_path = DIR
 
 debounce_delay = 10
@@ -23,12 +32,16 @@ for line in process1.stdout:
     parts = line.decode("utf-8").strip().split(" ")
     file_path = os.path.join(parts[0], parts[1])
 
-    # Get the current time
+    if "SSH_CLIENT" in os.environ:
+        ip_address = os.environ["SSH_CLIENT"].split()[0]
+        message = "The file {} was accessed by someone over SSH from IP address {}!".format(file_path, ip_address)
+    else:
+        message = "The file {} was accessed by someone locally!".format(file_path)
+
     current_time = time.time()
 
     last_access_time = last_access_times.get(file_path, 0)
     if current_time - last_access_time >= debounce_delay:
-        # Initialize the camera
         camera = cv2.VideoCapture(0)
 
         ret, frame = camera.read()
@@ -41,7 +54,7 @@ for line in process1.stdout:
         cv2.imwrite(image_file, frame)
 
         with open(image_file, "rb") as f:
-            payload = {"content": "The file {} was accessed by someone!".format(file_path)}
+            payload = {"content": message}
             files = {"file": f}
             response = requests.post(webhook_url, data=payload, files=files)
             if response.status_code != 200:
@@ -50,5 +63,3 @@ for line in process1.stdout:
         camera.release()
 
         last_access_times[file_path] = current_time
-
-
